c693c28a2b07188cdc1dde3279d1cc31c96ccb29
Harrison Wittke <harrison.wittke@student.uts.edu.au>
2020-10-29 01:14:56 +1100

    Finished Shin kinematics
    Forward and inverse kinematics done for shin mechanism - will need to be updated with transforms to account for hip rotations - current values are relative to Tmotor centres in plane of leg

--- Docs/Leg_Kinematics_v2/LegKinematics.m
+++ Docs/Leg_Kinematics_v2/LegKinematics.m
@@ -9,7 +9,7 @@ l3 = 309;
 l4 = 115;
 l5 = 190;
 l6 = 27.35;
-l7 = 40.153;
+l7 = 40.32;
 l8 = 189.795; % Spring Shin mechanism: +- 22.5 mm
 l9 = 55;
 l10 = 40.25;
@@ -18,12 +18,19 @@ l11 = 74.75;
 l = [l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11];
 
 p0 = [0, 0];
-p4 = [-13.988, -498.771];
+p4 = [-238.158, -384.332];
 
 [q, ikP, ikTheta] = main_ikine(l, p0, p4);
 rad2deg(q)
 
-[fkP, fkTheta] = main_fkine(l, p0, q)
+p8 = [-291.215, -369.844];
+
+[fkP, fkTheta] = main_fkine(l, p0, q);
+
+[q3, outP] = shin_ikine(l, ikP, p8, pi, ikTheta);
+rad2deg(q3)
+
+[outP2] = shin_fkine(l, fkP, pi, fkTheta, q3) 
 %% Inverse Kinematics - Main Mechanism (Points 0 - 4)
 function [q, outP, theta] = main_ikine(l, p0, p4)
     lr1 = distance(p0(1), p0(2), p4(1), p4(2));
@@ -60,16 +67,31 @@ function [q, outP, theta] = main_ikine(l, p0, p4)
     outP(5, 2) = outP(2, 2) - l(11) * sin(theta);
 end
 %% Inverse Kinematics - Shin Mechanism
-function [q, outP, theta] = shin_ikine(l, inP, p8, omega, theta)
+function [q3, outP] = shin_ikine(l, inP, p8, omega, theta)
     for (i = 1:5)
         for (j = 1:2)
             outP(i, j) = inP(i, j);
         end
     end
-    phi = % circle with vertical line through it on New IK3
-    outP(6, 1) = 
-    lr1 = distance(p(5, 1), p(5, 2), p8(1), p8(2));
-    alpha1 = angleCosineRule(l(8), l(7)
+    phi = pi/2 - theta;
+    outP(6, 1) = outP(5, 1) + l(6) * cos(omega - phi);
+    outP(6, 2) = outP(5, 2) - l(6) * sin(omega - phi);
+    lr1 = distance(outP(5, 1), outP(5, 2), p8(1), p8(2));
+    lr2 = distance(outP(6, 1), outP(6, 2), p8(1), p8(2));
+    
+    alpha1 = angleCosineRule(l(8), l(7), lr2);
+    alpha2 = angleCosineRule(lr1, lr2, l(6));
+    beta1 = angleCosineRule(l(9), lr1, (l(10) + l(5)));
+    beta2 = angleCosineRule(lr2, l(6), lr1);
+    alpha = alpha1 + alpha2;
+    beta = beta1 + beta2;
+    
+    outP(7, 1) = outP(6, 1) + l(7) * cos(omega - phi - (pi - alpha));
+    outP(7, 2) = outP(6, 2) - l(7) * sin(omega - phi - (pi - alpha));
+    outP(8, 1) = p8(1);
+    outP(8, 2) = p8(2);
+    
+    q3 = phi + theta + beta - (pi - alpha);
 end
 %% Forward Kinematics - Main Mechanism (Points 0 - 4)
 function [outP, theta] = main_fkine(l, p0, q)
@@ -95,6 +117,41 @@ function [outP, theta] = main_fkine(l, p0, q)
     theta = q(2) - (pi - alpha);
     outP(5, 1) = outP(2, 1) + l(11) * cos(theta);
     outP(5, 2) = outP(2, 2) - l(11) * sin(theta);
+end
+%% Forward Kinematics - Shin Mechanism
+function [outP] = shin_fkine(l, inP, omega, theta, q3) 
+    for (i = 1:5)
+        for (j = 1:2)
+            outP(i, j) = inP(i, j);
+        end
+    end
+    phi = pi/2 - theta;
+    infinity = omega - pi;
+    outP(6, 1) = outP(5, 1) + l(6) * cos(omega - phi);
+    outP(6, 2) = outP(5, 2) - l(6) * sin(omega - phi);
+    outP(7, 1) = outP(6, 1) + l(7) * cos(omega - phi - (pi - (q3 - infinity)));
+    outP(7, 2) = outP(6, 2) - l(7) * sin(omega - phi - (pi - (q3 - infinity)));
+    
+    lr1 = distance(outP(6, 1), outP(6, 2), outP(4, 1), outP(4, 2));
+    lr2 = distance(outP(7, 1), outP(7, 2), outP(4, 1), outP(4, 2));
+    alpha1 = angleCosineRule((l(10) + l(5)), l(6), lr1);
+    alpha2 = angleCosineRule(lr2, l(7), lr1);
+    beta1 = angleCosineRule(l(9), l(8), lr2);
+    beta2 = angleCosineRule(lr1, l(7), lr2);
+
+    lr1Angle = ((outP(6, 2) - outP(4, 2))/(outP(6, 1) - outP(4, 1)));
+    lr2Angle = ((outP(7, 2) - outP(4, 2))/(outP(7, 1) - outP(4, 1)));
+    if (lr2Angle > lr1Angle) 
+        alpha = alpha1 + alpha2;
+        beta = beta1 + beta2;
+        outP(8, 1) = outP(7, 1) + l(8) * cos(omega - phi - (pi - alpha) - (pi - beta));
+        outP(8, 2) = outP(7, 2) - l(8) * sin(omega - phi - (pi - alpha) - (pi - beta));
+    else
+        alpha = alpha2 - alpha1;
+        beta = beta1 - beta2;
+        outP(8, 1) = outP(7, 1) + l(8) * cos(omega - phi + (2*pi - alpha) - (pi - beta));
+        outP(8, 2) = outP(7, 2) - l(8) * sin(omega - phi + (2*pi - alpha) - (pi - beta));
+    end
 end
 %% Angle Cosine Rule
 function A = angleCosineRule(a, b, c)
