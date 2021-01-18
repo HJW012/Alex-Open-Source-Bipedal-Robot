f2daf3cd23a6042e749383925380bc3ee5e820b5
Harrison Wittke <16hawittke@gmail.com>
2021-01-18 23:00:31 +1100

    WOrkaround Joint for URDF purposes

--- Packages/alex_state_publisher/src/alex_state_publisher_node.cpp
+++ Packages/alex_state_publisher/src/alex_state_publisher_node.cpp
@@ -26,33 +26,16 @@ int main(int argc, char** argv)
 
   // gets the location of the robot description on the parameter server
   urdf::Model model;
-  urdf::Model model2;
   if (!model.initParam("robot_description"))
     return 1;
 
-  if (!model2.initParam("robot_description2"))
-    return 1;
 
   KDL::Tree tree;
-  KDL::Tree tree1;
   if (!kdl_parser::treeFromUrdfModel(model, tree)) {
     ROS_ERROR("Failed to extract kdl tree from xml robot description");
     return 1;
   }
 
-
-  if (!kdl_parser::treeFromUrdfModel(model2, tree1)) {
-    ROS_ERROR("Failed to extract kdl tree from xml robot description");
-    return 1;
-  }
-
-  if (!tree.addTree(tree1, "base_link")) {
-    std::cout << "ISSUES HERE" << std::endl;
-  }
-  std::cout << "Number of segments: " << tree.getNrOfSegments() << std::endl;
-  std::cout << "Number of joints: " << tree.getNrOfJoints() << std::endl;
-  std::cout << "Number of segments: " << tree1.getNrOfSegments() << std::endl;
-  std::cout << "Number of joints: " << tree1.getNrOfJoints() << std::endl;
 
   alex_state_publisher::MimicMap mimic;
   for (std::map< std::string, urdf::JointSharedPtr >::iterator i = model.joints_.begin(); i != model.joints_.end(); i++) {
@@ -66,16 +49,4 @@ int main(int argc, char** argv)
   ros::spin();
 
   return 0;
-}
-
-void completeTree() {
-  KDL::Chain chain1; //Chain containing link2, link4, link5
-  KDL::Chain chain2; //Chain containing link1, link3
-
-  KDL::Joint rz = KDL::Joint(KDL::Joint::RotZ);
-
-  //Links 1 and 2 are already created in the URDF
-
-
-
 }
